ffe4b104bcc650455b376da8f2e7a70b95c4015f
Sneha Narasimhan <sbnarasimhan@gmail.com>
2016-07-15 10:28:15 -0700

    Add formality function and script.

---
+++ js/formality.js
@@ -0,0 +1,15 @@
+// Takes in string of words and splits to array of words.
+
+function analyzeFormality(str){
+
+	console.log(str);
+}
+
+// FOR each word in the array, compare the word to formality library.
+	// check if it's included
+	// if it included, increment style type
+		// push the word and the comments from the library to separate array.
+
+//print out the separate array of suggestions.
+
+
