69a374e81ddd923b7303d165c6366624cff878c7
Robin Soubry <robin.soubry@gmail.com>
2016-07-14 21:33:07 -0700

    Refactor ajax call to tone analyzer

--- js/main.js
+++ js/main.js
@@ -14,21 +14,3 @@ $(document).ready(function(){
     analyzeTone(body);
   })
 })
-
-function analyzeTone(str){
-  var ajaxRequest = $.ajax({
-      url: "http://sentimail-api.herokuapp.com/tone",
-      method: "POST",
-      data: {content: str},
-      });
-
-    ajaxRequest.done(createToneChart);
-
-    ajaxRequest.fail(function(){
-      console.log("Ajax call failed");
-    });
-}
-
-function createToneChart(responseData) {
-  console.log(responseData);
-}
