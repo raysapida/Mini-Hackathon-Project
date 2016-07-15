5a54883f585d60b248e7d44ed953fdee49287dff
Sneha Narasimhan <sbnarasimhan@gmail.com>
2016-07-15 12:08:36 -0700

    Add appendFormalitySuggestions function to append formality indicator to index.

--- js/formality.js
+++ js/formality.js
@@ -9,35 +9,35 @@ function analyzeFormality(str){
 
 var formalityLibrary = [
 
-	{
-		word: "Respectfully,",
-		type: "Formal",
-		comment: ""
-	},
+{
+	word: "Respectfully,",
+	type: "Formal",
+	comment: ""
+},
 
-	{
-		word: "got",
-		type: "Informal",
-		comment: "Consider using have"
-	},
+{
+	word: "got",
+	type: "Informal",
+	comment: "Consider using have"
+},
 
-	{
-		word: "cheers",
-		type: "Informal",
-		comment: "Consider changing to 'respectfully'."
-	},
+{
+	word: "cheers",
+	type: "Informal",
+	comment: "Consider changing to 'respectfully'."
+},
 
-	{
-		word: "&",
-		type: "Informal",
-		comment: "Consider changing to 'and'."
-	},
+{
+	word: "&",
+	type: "Informal",
+	comment: "Consider changing to 'and'."
+},
 
-	{
-		word: "Mr.",
-		type: "formal",
-		comment: ""
-	}	
+{
+	word: "Mr.",
+	type: "formal",
+	comment: ""
+}	
 
 ];
 
@@ -60,14 +60,27 @@ function formalityCounter(obj){
 		}
 	}
 	var formalityScore = ((formalCount/(formalCount+informalCount)) * 100 );
-	formalityIndicator(formalityScore, wordSuggestions);
+	appendFormalityIndicator(formalityScore);
+	appendFormalitySuggestions(wordSuggestions);
 }
 
-function formalityIndicator(formalityScore, wordSuggestions){
+function appendFormalityIndicator(formalityScore){
 
 	formalityHTML = "<h3>"+ formalityScore + "% <small>Formality Score</small></h3>";
 
 	$("#formality-score").append(formalityHTML);
 
+}
+
+function appendFormalitySuggestions(wordSuggestions){
+	var suggestionHTML = "<hr><h4>How you can improve:</h4><dl class='dl-horizontal'>"
+	
+	for (var i = 0; i < wordSuggestions.length; i++){
+		suggestionHTML = suggestionHTML.concat("<dt>" + wordSuggestions[i].word + "</dt>" + "<dd>" + wordSuggestions[i].comment + "</dd>")
+	}
+
+	suggestionHTML = suggestionHTML.concat("</dl>");
+
+	$("#formality-score").append(suggestionHTML);
 }
 
