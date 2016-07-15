0025796e19eae8e8473b48f664979469087e35f5
Sneha Narasimhan <sbnarasimhan@gmail.com>
2016-07-15 11:55:25 -0700

    Add floor to round down seconds.

--- js/time_to_read.js
+++ js/time_to_read.js
@@ -2,13 +2,13 @@ function timeToRead(str){
   var wordsPerMinute = 225;
   var wordArray = str.split(' ');
   var timeInSeconds = (wordArray.length / wordsPerMinute) * 60;
-  appendTimeToRead(timeInSeconds);
+  appendTimeToRead(Math.floor(timeInSeconds));
 }
 
 
 
 function appendTimeToRead(seconds){
-  timeToReadHTML = "<h3>" + seconds + "</h3>"
+  timeToReadHTML = "<h3>" + seconds + "<small> Seconds</small></h3>"
 
   $("#time-to-read").append(timeToReadHTML);
 }
