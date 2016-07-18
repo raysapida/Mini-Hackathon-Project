4c254b362860c2616d37dac99cc90090c7ce92cf
Raymond Sapida <ray.sapida@gmail.com>
2016-07-17 20:26:01 -0700

    Add test for removeKeyWords function

--- test/word_count_test.js
+++ test/word_count_test.js
@@ -5,7 +5,6 @@ describe("word_count.js",function(){
 
   it("wordCount returns an array with five elements",function(done){
     var input = "Mr. Jones, I have been researching our choices for internet providers over the past week, & I wanted to update you on my progress. We got two options: H.C. Cable & Toll South. Both offer business plans, & I will go over the business pricing of each plan at the meeting on Tuesday. Both of the business options I listed have comparable speed & data usage offerings as well. I called your personal provider, GoGo Satellite, but they did not have any business offerings. They primarily do residential internet service. I will talk with Joe & Susan in IT about these options & get their suggestions. I will also send out meeting requests to everyone, including Mr. Morris in operations. If you have any questions prior to the meeting, please let me know."
-    console.log(wordCount(input));
     expect(wordCount(input).length).to.equal(5)
     done();
   });
@@ -20,4 +19,16 @@ describe("word_count.js",function(){
     expect(topWords[4][0]).to.equal('on')
     done();
   });
+
+  it("removeKeyWords returns an array without keywords",function(done){
+    var input = "Mr. Jones, I have been researching our choices for internet providers over the past week, & I wanted to update you on my progress. We got two options: H.C. Cable & Toll South. Both offer business plans, & I will go over the business pricing of each plan at the meeting on Tuesday. Both of the business options I listed have comparable speed & data usage offerings as well. I called your personal provider, GoGo Satellite, but they did not have any business offerings. They primarily do residential internet service. I will talk with Joe & Susan in IT about these options & get their suggestions. I will also send out meeting requests to everyone, including Mr. Morris in operations. If you have any questions prior to the meeting, please let me know."
+    var strArray = input.toLowerCase().split(' ');
+    var toRemoveArray = ['&', 'and', 'I', 'the' , 'have', 'to', 'will', 'am', 'are', 'i'];
+    var result = removeKeyWords(strArray, toRemoveArray);
+    toRemoveArray.forEach(function(word){
+      expect(result).to.not.include(word);
+    })
+    done();
+  });
+
 });
