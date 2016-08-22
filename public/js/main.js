958d3d20cb625fd3eb2e80835c1295cd4f8d149f
Olya Royall <olya.vgrig+git@gmail.com>
2016-08-21 19:53:33 -0700

    add input change event listener, take out button grey out

--- public/js/main.js
+++ public/js/main.js
@@ -1,16 +1,15 @@
 $(document).ready(function(){
+  $('#text-to-analyze').on('input', function(e){
+      console.log(" text has been changed!");
+    })
+
   $('#analyze').on('click', function(event){
-    $( this ).css({'background-color': 'grey'});
-    $( this ).prop('disabled', true);
     event.preventDefault();
-    if ($(this).disabled !== true) {
       showResults();
       var body = document.getElementById('text-to-analyze').value
       wordCount(body);
       analyzeTone(body);
       timeToRead(body);
       analyzeFormality(body);
-    }
-
   })
 })
